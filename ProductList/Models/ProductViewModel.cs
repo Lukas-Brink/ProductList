55d9b375086693a828c9a15c2992e7e62168f020
Nobody <Nobody@DESKTOP-FPH7G7L>
2025-04-30 23:15:28 +0200

    Add project files.

---
+++ ProductList/Models/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductList.Models
+{
+    public class ProductViewModel
+
+
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Serial number cannot exceed 10 digits.")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Serial number must be numeric.")]
+        public string Serial { get; set; }
+        public int SelectedSupplierId { get; set; }
+        public int SelectedCategoryId { get; set; }
+        public int SelectedProductId { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Supplier> Suppliers { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ ProductList/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductList
+{
+    public class Product
+    {
+    }
+}
